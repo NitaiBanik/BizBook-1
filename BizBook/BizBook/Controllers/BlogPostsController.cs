295369bec447227fb8edd20949828fd7caffc73e
Atmospheres <BAJakubowski@gmail.com>
2018-10-11 10:26:33 -0500

    blogpost model and controller added

---
+++ BizBook/BizBook/Controllers/BlogPostsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BizBook.Data;
+using BizBook.Models;
+
+namespace BizBook.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlogPostsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BlogPosts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BlogPost.ToListAsync());
+        }
+
+        // GET: BlogPosts/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogPost = await _context.BlogPost
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BlogPosts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Title,Content,PubDate,LastEdited,IsPublished")] BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blogPost);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogPost = await _context.BlogPost.FindAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("ID,Title,Content,PubDate,LastEdited,IsPublished")] BlogPost blogPost)
+        {
+            if (id != blogPost.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(blogPost);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogPostExists(blogPost.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogPost = await _context.BlogPost
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var blogPost = await _context.BlogPost.FindAsync(id);
+            _context.BlogPost.Remove(blogPost);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogPostExists(string id)
+        {
+            return _context.BlogPost.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ BizBook/BizBook/Models/BlogPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizBook.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PubDate { get; set; }
+        public DateTime LastEdited { get; set; }
+        public bool IsPublished { get; set; } = true;
+
+
+
+    }
+}
